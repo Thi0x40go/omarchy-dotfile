ee0fab7d585200b804fe50d44147d04c34f4419e
Thiago Pinheiro <thiago.pinheiro@apphealth.com.br>
2026-06-17 13:30:07 -0300

    chore: remove hyprmodoro and add Task Bar Hero configs

--- waybar/hyprmodoro-toggle.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-state=$(hyprctl hyprmodoro:getState 2>/dev/null | tr -d '\r\n')
-
-if [ "$state" = "STOPPED" ] || [ -z "$state" ]; then
-    hyprctl dispatch hyprmodoro:start
-else
-    hyprctl dispatch hyprmodoro:pause
-fi
